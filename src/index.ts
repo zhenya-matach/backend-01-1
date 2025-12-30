df2542141218a0cf40a35b3a5c9b5d45eadf91ff
Yauheni Matach <zhenya.matach@gmail.com>
2025-12-30 17:55:24 +0300

    add endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
-import express from 'express'
+import express, {Request, Response} from 'express'
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello Incubator!!!')
+const products = [{title: 'tomato'},{title: 'orange'}];
+const addresses = [{value: 'Nezalezhnasti 12'},{value: 'Selickaga 11'}];
+
+app.get('/products', (req: Request, res: Response) => {
+    res.send(products);
+})
+
+app.get('/addresses', (req: Request, res: Response) => {
+    res.send(addresses);
+})
+app.get('/', (req: Request, res: Response) => {
+    let helloMassage = 'Hello World!!!';
+    res.send(helloMassage);
 })
 
 app.listen(port, () => {
